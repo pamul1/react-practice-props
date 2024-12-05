21efa34b89f2a79eb866461f6456b6bc2141f2f0
pamul1 <109163253+pamul1@users.noreply.github.com>
2024-12-04 21:04:14 -0600

    Commit4

--- src/Component/ProductCard.jsx
+++ src/Component/ProductCard.jsx
@@ -4,13 +4,13 @@ export const ProductCard = ({ product }) => {
 
     return (
         <div>
-            {product.map((product) => {
+            {product.map((product) => (
                 <div key={product.name} className="card m-5">
                     <div className="card-header">{product.name}</div>
                     <div className="card-body">{product.price}</div>
                     <div className="card-footer">{product.stock}</div>
                 </div>
-            })}
+            ))}
         </div>
     )
 }
